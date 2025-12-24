1a6830e973bae04c87f925e533865fb94d57b116
Erik Saule <esaule@charlotte.edu>
2025-12-23 19:05:51 -0500

    adding TTS in animation, better layouts

--- Animations/MST.py
+++ Animations/MST.py
@@ -6,15 +6,16 @@ import json
 import sys
 
 #Helper function used to randomly populate a graph's edges and nodes. 
-def randomEdgeGen(vCount: int, edgesArr: list):
+def randomEdgeGen(vCount: int, edgesArr: list, degree: int = 1):
     for i in range(vCount):
-        while(True): #Loop is used to ensure no duplicate edges.
-            randEdge = random.randrange(vCount)
-            if((randEdge, i) in edgesArr or i == randEdge):
-                continue
-            else:
-                break
-        edgesArr.append((i, randEdge)) #Add edge tuple to array of edges.        
+        for _ in range(degree):
+            while(True): #Loop is used to ensure no duplicate edges.
+                randEdge = random.randrange(vCount)
+                if((randEdge, i) in edgesArr or i == randEdge):
+                    continue
+                else:
+                    break
+            edgesArr.append((i, randEdge)) #Add edge tuple to array of edges.        
 
 
         
@@ -22,18 +23,18 @@ def makegraph():
         #Scene graph parameters.
         vCount = 8
         pos = {
-            0:(0,4,0),
+            0:(0,3.5,0),
             1:(-1.5,1.5,0),
             2:(1,1.5,0),
             3:(-1.5,-1.5,0),
             4:(1,-1.5,0),
-            5:(0,-4,0),
-            6:(-4,0,0),
-            7:(4,0,0)
+            5:(0,-3.5,0),
+            6:(-3.5,0,0),
+            7:(3.5,0,0)
         }
         verticesArr = [i for i in range(vCount)]
         edgesArr = []
-        randomEdgeGen(vCount, edgesArr)
+        randomEdgeGen(vCount, edgesArr, 2)
         
         #Scene graph creation
         sceneNXGraph = nx.Graph()
@@ -63,7 +64,10 @@ def make_animation(sceneNXGraph):
         mst = tree.minimum_spanning_edges(mstGraph, data = True, algorithm="prim")
         mstEdges = list(mst)
         animation_steps = []
-        animation_steps.append( [ {"applyon":"G", "data": {"type": "vertexcolor", "vertex":mstEdges[0][0], "color":[1.,0.,0.,1.]} } ] )
+        mark = {}
+        tts0 = {"applyon":"tts", "data":{"type":"say", "text": f"Marking {mstEdges[0][0]} as initial vertex reached."}}
+        mark[mstEdges[0][0]] = True
+        animation_steps.append( [ {"applyon":"G", "data": {"type": "vertexcolor", "vertex":mstEdges[0][0], "color":[1.,0.,0.,0.6]} }, tts0] )
         for edge in mstEdges:
                 type = "edgecolor"
                 if(edge[0], edge[1]) in sceneNXGraph.edges: 
@@ -75,16 +79,26 @@ def make_animation(sceneNXGraph):
                 color = [0.,1.,0.,1.]
                 tts1 = {"applyon":"tts", "data":{"type":"say", "text": f"Adding edge ({src}, {dst}) as part of the spanning tree."}}
                 animation_steps.append( [ {"applyon": "G", "data": {"type": type, "src": src, "dst":dst, "color": color}} , tts1] )
-                type = "vertexcolor"
+                
+                
                 vertex = edge[0]
-                color = [1.,0.,0.,1.]
-                ahhh = {"applyon":"G", "data": {"type": type, "vertex": vertex, "color": color}}
-                type = "vertexcolor"
+                if vertex not in mark:
+                    type = "vertexcolor"
+                    color = [1.,0.,0.,0.6]
+                    ahhh = {"applyon":"G", "data": {"type": type, "vertex": vertex, "color": color}}
+                    tts2 = {"applyon":"tts", "data":{"type":"say", "text": f"Marking {vertex} as reached."}}
+                    mark[vertex] = True
+                    animation_steps.append( [ ahhh, tts2] )
+                    
                 vertex = edge[1]
-                color = [1.,0.,0.,1.]
-                bhhh = {"applyon":"G", "data": {"type": type, "vertex": vertex, "color": color}}
-                tts2 = {"applyon":"tts", "data":{"type":"say", "text": f"Marking both {src} and {dst} as reached."}}
-                animation_steps.append( [ ahhh, bhhh, tts2] )
+                if vertex not in mark:
+                    type = "vertexcolor"
+                    color = [1.,0.,0.,0.6]
+                    ahhh = {"applyon":"G", "data": {"type": type, "vertex": vertex, "color": color}}
+                    tts2 = {"applyon":"tts", "data":{"type":"say", "text": f"Marking {vertex} as reached."}}
+                    mark[vertex] = True
+                    animation_steps.append( [ ahhh, tts2] )
+
         #print (animation_steps)
 
         return animation_steps
